baa35b251b421678b0cf49d22053aeaa5ca8c803
nazi <nazipaeez777@gmail.com>
2025-03-01 20:10:06 +0330

    Config env in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import {ConfigModule} from '@nestjs/config'
 
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    UsersModule, 
+    AuthModule
+
+  ],
   controllers: [],
   providers: [],
 })
