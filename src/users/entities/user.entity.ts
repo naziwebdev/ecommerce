3a971bcb1af4b22091cedc76d4d66f48995b3f7c
nazi <nazipaeez777@gmail.com>
2025-03-01 20:41:11 +0330

    Create user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,47 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { UserRoleEnum } from '../enums/user-role-enum';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  username: string;
+
+  @Column({ nullable: false, unique: true })
+  phone: string;
+
+  @Column({ nullable: false })
+  @Exclude()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRoleEnum,
+    default: UserRoleEnum.USER,
+    nullable: false,
+  })
+  role: UserRoleEnum;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
