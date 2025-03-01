3dca1f1ebbba53c2e599581fdbf63e892ec23aa7
nazi <nazipaeez777@gmail.com>
2025-03-01 20:15:30 +0330

    Config connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import {ConfigModule} from '@nestjs/config'
+import {TypeOrmModule} from '@nestjs/typeorm'
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal:true}),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     UsersModule, 
     AuthModule
 
