8725b5e0d6f77e26fea6245eb857689dc6d09768
nazi <nazipaeez777@gmail.com>
2025-03-04 23:24:19 +0330

    Create get user decorator

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -2,12 +2,17 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { JwtService } from '@nestjs/jwt';
 import { Reflector } from '@nestjs/core';
-
+//for auth-guard and set user on request such as express auth middleware => we dont need to middleware that set user on request
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = request.headers.authorization?.split(' ')[1];
     if (!token) {
