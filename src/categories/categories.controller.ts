a61860e13dfbcafac540c19e93566713197b22a2
nazi <nazipaeez777@gmail.com>
2025-03-05 21:31:17 +0330

    Create create-category api

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Res,
+  Body,
+  HttpStatus,
+  Param,
+  UseGuards,
+  Post,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { JwtAuthGuard } from 'src/guards/auth.guard';
+import { RoleGuard } from 'src/guards/role.guard';
+import { Response } from 'express';
+import { CreateCategoryDto } from './dtos/create-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  async create(@Body() body: CreateCategoryDto, @Res() res: Response) {
+    const newCategory = await this.categoriesService.create(body);
+
+    return res.status(HttpStatus.CREATED).json({
+      data: newCategory,
+      statusCode: HttpStatus.CREATED,
+      message: 'category created successfully',
+    });
+  }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
+import { CreateCategoryDto } from './dtos/create-category.dto';
+import { UpdateCategoryDto } from './dtos/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
@@ -10,4 +12,16 @@ export class CategoriesService {
     private categoriesRepository: Repository<Category>,
   ) {}
 
+  async create(createCategoryDto: CreateCategoryDto) {
+    try {
+      const category =
+        await this.categoriesRepository.create(createCategoryDto);
+
+      return await this.categoriesRepository.save(category);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Failed to create category/title must be unique',
+      );
+    }
+  }
 }
