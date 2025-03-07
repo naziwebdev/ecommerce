f5e41120b71a37c121cf6b6a99fb85e42c87f77f
nazi <nazipaeez777@gmail.com>
2025-03-07 17:10:11 +0330

    Create create-address api

--- src/addresses/addresses.controller.ts
+++ src/addresses/addresses.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
 import { AddressesService } from './addresses.service';
+import { JwtAuthGuard } from 'src/guards/auth.guard';
+import { CreateAddressDto } from './dtos/create-address.dto';
+import { Response } from 'express';
+import { GetUser } from 'src/decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('addresses')
 export class AddressesController {
   constructor(private readonly addressesService: AddressesService) {}
+
+  @Post('/')
+  @UseGuards(JwtAuthGuard)
+  async create(
+    @Body() body: CreateAddressDto,
+    @GetUser() user: User,
+    @Res() res: Response,
+  ) {
+    const address = await this.addressesService.create(body, user);
+
+    return res.status(HttpStatus.CREATED).json({
+      data: address,
+      statusCode: HttpStatus.CREATED,
+      message: 'address created successfully',
+    });
+  }
 }
--- src/addresses/addresses.service.ts
+++ src/addresses/addresses.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Address } from './entities/address.entity';
+import { CreateAddressDto } from './dtos/create-address.dto';
+import { User } from 'src/users/entities/user.entity';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
-export class AddressesService {}
+export class AddressesService {
+  constructor(
+    @InjectRepository(Address)
+    private addressesRepository: Repository<Address>,
+  ) {}
+
+  async create(createAddressDto: CreateAddressDto, user: User) {
+    const address = await this.addressesRepository.create({
+      ...createAddressDto,
+      user: user,
+    });
+
+    const savedAddress = await this.addressesRepository.save(address);
+
+    savedAddress.user = plainToInstance(User, savedAddress.user);
+
+    return savedAddress;
+  }
+}
