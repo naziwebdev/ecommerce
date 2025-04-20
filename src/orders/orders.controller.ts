8ccba05bc6d9f0d471b87081e8491f41f4bd5b39
nazi <nazipaeez777@gmail.com>
2025-04-21 00:22:55 +0330

    Develope get user orders api

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Query,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { GetUser } from 'src/decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { JwtAuthGuard } from 'src/guards/auth.guard';
+import { Response } from 'express';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @Get('/')
+  @UseGuards(JwtAuthGuard)
+  async getUserCart(
+    @GetUser() user: User,
+    @Query('page') page: string,
+    @Query('limit') limit: string,
+    @Res() res: Response,
+  ) {
+    const cartItems = await this.ordersService.getUserOrders(
+      parseInt(page),
+      parseInt(limit),
+      user,
+    );
+
+    return res.status(HttpStatus.OK).json({
+      data: cartItems,
+      statusCode: HttpStatus.OK,
+      message: 'cartItems send successfully',
+    });
+  }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -49,9 +49,22 @@ export class OrdersService {
     return await this.orderItemsRepository.save(newOrderItem);
   }
 
+  async getUserOrders(page: number = 1, limit: number = 2, user: User) {
+    const userId = user.id;
+    const [orders, total] = await this.ordersRepository
+      .createQueryBuilder('order')
+      .leftJoinAndSelect('order.orderItems', 'orderItems')
+      .where('order.userId = :userId', { userId })
+      .skip((page - 1) * limit)
+      .take(limit)
+      .getManyAndCount();
+
+    return { orders, total };
+  }
+
   async findByAuthority(authority: string) {
     const order = await this.ordersRepository.findOne({
-      where: {authority },
+      where: { authority },
     });
 
     return order;
